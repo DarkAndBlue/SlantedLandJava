d850dd773d3b6688d88cc487b9dbc16bca7bcf97
DarkAndBlue <13067258+DarkAndBlue@users.noreply.github.com>
2022-03-31 23:37:44 +0200

    Made program runnable and created a window to test Network

--- src/main/java/slantedland/SlantedLand.java
+++ src/main/java/slantedland/SlantedLand.java
@@ -1,11 +1,20 @@
 package slantedland;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class SlantedLand {
   static Random rand = new Random();
   
-  //# TODO: changes x input type between ndarray and float64
+  public static void main(String[] args) {
+    startGan();
+  }
+  
+//#  changes x input type between ndarray and float64
 //  def sigmoid(x):
 //    return np.exp(x) / (1.0 + np.exp(x))
   public static double[] sigmoid(double[] x) {
@@ -20,7 +29,7 @@ public class SlantedLand {
     return Math.exp(x) / (1.0 + Math.exp(x));
   }
   
-  //    # real samples
+//    # real samples
 //  faces: list[ndarray] = [np.array([1, 0, 0, 1]),
 //    np.array([0.9, 0.1, 0.2, 0.8]),
 //    np.array([0.9, 0.2, 0.1, 0.8]),
@@ -28,7 +37,7 @@ public class SlantedLand {
 //    np.array([0.8, 0.2, 0.1, 0.9])]
   static double[][] faces = { { 1, 0, 0, 1 }, { 0.9, 0.1, 0.2, 0.8 }, { 0.9, 0.2, 0.1, 0.8 }, { 0.8, 0.1, 0.2, 0.9 }, { 0.8, 0.2, 0.1, 0.9 } };
   
-  //  noise: list[np.ndarray] = [np.random.randn(2, 2) for i in range(20)]
+//  noise: list[np.ndarray] = [np.random.randn(2, 2) for i in range(20)]
   /*
     [array([[ 1.30345511,  0.87838275], [-1.55296293,  1.37779997]]),
     array([[-2.26870357, -2.20717963], [-0.49189746, -0.65753657]]),
@@ -64,7 +73,7 @@ public class SlantedLand {
     }
   }
   
-  //  def generate_random_image():
+//  def generate_random_image():
 //    return [np.random.random(), np.random.random(), np.random.random(), np.random.random()]
   public static double[] generate_random_image() {
     double[] x = new double[4];
@@ -74,14 +83,83 @@ public class SlantedLand {
     return x;
   }
   
-  //  class Discriminator:
-  class Discriminator {
+  static class np {
+    public static double[] dot(double a, double[] b) {
+      return multiply(a, b);
+    }
+    
+    public static double dot(double a[], double[] b) {
+      if (a.length != b.length)
+        throw new RuntimeException("Length of arrays doesn't match " + a.length + " != " + b.length);
+      
+      double sum = 0;
+      for (int i = 0; i < a.length; i++) {
+        sum += a[i] * b[i];
+      }
+      return sum;
+    }
+    
+    public static double[] log(double[] input) {
+      double[] result = new double[input.length];
+      for (int i = 0; i < input.length; i++) {
+        result[i] = Math.log(result[i]);
+      }
+      return result;
+    }
+    
+    public static double[] multiply(double a, double[] b) {
+      double[] c = new double[b.length];
+      for (int i = 0; i < b.length; i++) {
+        c[i] = b[i] * a;
+      }
+      return c;
+    }
+    
+    public static double[] multiply(double[] a, double[] b) {
+      if (a.length != b.length)
+        throw new RuntimeException("Length of arrays doesn't match!" + a.length + " != " + b.length);
+      
+      double[] c = new double[b.length];
+      for (int i = 0; i < a.length; i++) {
+        c[i] = a[i] * b[i];
+      }
+      
+      return c;
+    }
+  }
+  
+  public static double[] subtractReverse(double a, double[] b) {
+    double[] result = new double[b.length];
+    for (int i = 0; i < b.length; i++) {
+      result[i] = a - b[i];
+    }
+    return result;
+  }
+  
+  public static double[] inverse(double[] input) {
+    double[] result = new double[input.length];
+    for (int i = 0; i < input.length; i++) {
+      result[i] = -result[i];
+    }
+    return result;
+  }
+  
+  public static double sum(double[] n) {
+    double sum = 0;
+    for (int i = 0; i < n.length; i++) {
+      sum += n[i];
+    }
+    return sum;
+  }
+  
+//  class Discriminator:
+  static class Discriminator {
     //  weights: ndarray # [ 0.49671415 -0.1382643   0.64768854  1.52302986]
     double[] weights;
     //  biases: float64
     double bias;
     
-    //  def __init__(self):
+//  def __init__(self):
 //    # self.weights = np.array([0.0 for i in range(4)])
 //    # self.bias = 0.0
 //  self.weights = np.array([np.random.normal() for i in range(4)])
@@ -96,32 +174,32 @@ public class SlantedLand {
     }
     
     
-    //    # TODO: x is a ndarray and some times a float. Check which one get called from where
-//    # TODO: if x is float return value is ndarray, if x is ndarray return value is float64
+//    # if x is float return value is ndarray, if x is ndarray return value is float64
 //    def forward(self, x):
 //      # Forward pass
-//        return sigmoid(np.dot(x, self.weights) + self.bias)  # TODO: changes classes depending on x (float64 and ndarray)
-    double[] forward(double x) {
-      throw new RuntimeException("not implemented");
 //        return sigmoid(np.dot(x, self.weights) + self.bias)
+    double[] forward(double x) {
+      double[] a = np.dot(x, weights);
+      for (int i = 0; i < a.length; i++) {
+        a[i] += bias;
+      }
+      return sigmoid(a);
     }
     
     double forward(double[] x) {
-      throw new RuntimeException("not implemented");
-//        return sigmoid(np.dot(x, self.weights) + self.bias)
+      return sigmoid(np.dot(x, weights) + bias);
     }
     
-    //  def error_from_image(self, image: ndarray) -> float64:
+//  def error_from_image(self, image: ndarray) -> float64:
 //  prediction: float64 = self.forward(image)
 //    # We want the prediction to be 1, so the error is -log(prediction)
 //        return -np.log(prediction)
     double error_from_image(double[] image) {
       double prediction = forward(image);
-      throw new RuntimeException("not implemented");
-      //        return -np.log(prediction)
+      return -Math.log(prediction);
     }
     
-    //  def derivatives_from_image(self, image: ndarray) -> tuple:
+//  def derivatives_from_image(self, image: ndarray) -> tuple:
 //  prediction: float64 = self.forward(image)
 //  derivatives_weights: ndarray = -image * (1 - prediction)
 //  derivative_bias: float64 = -(1 - prediction)
@@ -136,7 +214,7 @@ public class SlantedLand {
       return new Object[] { derivatives_weights, derivative_bias };
     }
     
-    //  def update_from_image(self, x: ndarray):
+//  def update_from_image(self, x: ndarray):
 //  ders: tuple = self.derivatives_from_image(x)
 //  self.weights -= learning_rate * ders[0]
 //  self.bias -= learning_rate * ders[1]
@@ -150,17 +228,16 @@ public class SlantedLand {
       bias -= learning_rate * derivative_bias;
     }
     
-    //  def error_from_noise(self, noise: float) -> ndarray:
+//  def error_from_noise(self, noise: float) -> ndarray:
 //  prediction: ndarray = self.forward(noise)
 //    # We want the prediction to be 0, so the error is -log(1-prediction)
 //        return -np.log(1 - prediction)  # ndarray
     double[] error_from_noise(double noise) {
       double[] prediction = forward(noise);
-      throw new RuntimeException("not implemented");
-      //        return -np.log(1 - prediction)  # ndarray
+      return inverse(np.log(subtractReverse(1, prediction)));  // ndarray
     }
     
-    //  def derivatives_from_noise(self, noise: ndarray) -> tuple:
+//  def derivatives_from_noise(self, noise: ndarray) -> tuple:
 //  prediction: float64 = self.forward(noise)
 //  derivatives_weights: ndarray = noise * prediction
 //  derivative_bias: float64 = prediction
@@ -175,7 +252,6 @@ public class SlantedLand {
       return new Object[] { derivatives_weights, derivative_bias };
     }
     
-    //  
 //  def update_from_noise(self, noise: ndarray):
 //  ders: tuple = self.derivatives_from_noise(noise)
 //  self.weights -= learning_rate * ders[0]
@@ -191,15 +267,15 @@ public class SlantedLand {
     }
   }
   
-  //  class Generator:
-  class Generator {
-    //  weights: ndarray # [-0.23413696  1.57921282  0.76743473 -0.46947439]
+//  class Generator:
+  static class Generator {
+//  weights: ndarray # [-0.23413696  1.57921282  0.76743473 -0.46947439]
     double[] weights;
     
-    //  biases: ndarray # [ 0.54256004 -0.46341769 -0.46572975  0.24196227]
+//  biases: ndarray # [ 0.54256004 -0.46341769 -0.46572975  0.24196227]
     double[] biases;
     
-    //  def __init__(self):
+//  def __init__(self):
 //  self.weights = np.array([np.random.normal() for i in range(4)])
 //  self.biases = np.array([np.random.normal() for i in range(4)])
     public Generator() {
@@ -213,7 +289,7 @@ public class SlantedLand {
       }
     }
     
-    //  def forward(self, z: float) -> ndarray:
+//  def forward(self, z: float) -> ndarray:
 //    # Forward pass
 //        return sigmoid(z * self.weights + self.biases)
     double[] forward(double z) {
@@ -224,7 +300,7 @@ public class SlantedLand {
       return sigmoid(z_weights);
     }
     
-    //  def error(self, z: float, discriminator: Discriminator) -> float64:
+//  def error(self, z: float, discriminator: Discriminator) -> float64:
 //  x: ndarray = self.forward(z)
 //    # We want the prediction to be 0, so the error is -log(1-prediction)
 //  y: float64 = discriminator.forward(x)
@@ -235,7 +311,7 @@ public class SlantedLand {
       return -Math.log(y);
     }
     
-    //  def derivatives(self, z: float, discriminator: Discriminator) -> tuple:
+//  def derivatives(self, z: float, discriminator: Discriminator) -> tuple:
 //  discriminator_weights: ndarray = discriminator.weights
 //#  discriminator_bias: float64 = discriminator.bias
 //  x: ndarray = self.forward(z)
@@ -249,24 +325,28 @@ public class SlantedLand {
 //      double discriminator_bias = discriminator.bias;
       double[] x = forward(z);
       double y = discriminator.forward(x);
-      throw new RuntimeException("not implemented");
-//      double[] factor = -(1 - y) * discriminator_weights * x * (1 - x);
-//      double[] derivatives_weights = new double[4];
-//      for (int i = 0; i < derivatives_weights.length; i++) {
-//        derivatives_weights[i] = factor[i] * z;
-//      }
-//      double[] derivative_bias = factor;
-//      return new Object[] { derivatives_weights, derivative_bias };
+      
+      double a = -(1 - y);
+      double[] b = np.multiply(a, discriminator_weights);
+      b = np.multiply(b, x);
+      double[] factor = np.multiply(b, subtractReverse(1, x));
+      
+      double[] derivatives_weights = new double[4];
+      for (int i = 0; i < derivatives_weights.length; i++) {
+        derivatives_weights[i] = factor[i] * z;
+      }
+      double[] derivative_bias = factor;
+      return new Object[] { derivatives_weights, derivative_bias };
     }
     
-    //  def update(self, z: float, discriminator: Discriminator):
+//  def update(self, z: float, discriminator: Discriminator):
 //  error_before: float64 = self.error(z, discriminator)
 //  ders: tuple = self.derivatives(z, discriminator)
 //  self.weights -= learning_rate * ders[0]
 //  self.biases -= learning_rate * ders[1]
 //  error_after: float64 = self.error(z, discriminator)
     void update(double z, Discriminator discriminator) {
-      double error_before = error(z, discriminator);
+//      double error_before = error(z, discriminator);
       Object[] ders = derivatives(z, discriminator);
       double[] derivatives_weights = (double[]) ders[0];
       double[] derivative_bias = (double[]) ders[1];
@@ -276,48 +356,190 @@ public class SlantedLand {
       for (int i = 0; i < weights.length; i++) {
         biases[i] -= learning_rate * derivative_bias[i];
       }
-      double error_after = error(z, discriminator);
+//      double error_after = error(z, discriminator);
     }
   }
   
-  //    
 //    # Set random seed
 //np.random.seed(42)
 //  
 //  # Hyperparameters
 //  learning_rate: float = 0.01
-  double learning_rate;
+  static double learning_rate = 0.01;
+  
 //  epochs: int = 1000
-//    
+  static int epochs = 1000;
+  
 //    # The GAN
+  public static void startGan() {
 //  D = Discriminator()
 //  G = Generator()
-//
+    Discriminator d = new Discriminator();
+    Generator g = new Generator();
+
+
 //# For the error plot
 //  errors_discriminator: list[float64] = []
 //  errors_generator: list[float64] = []
-//    
+    List<Double> errors_discriminator = new ArrayList<>();
+    List<Double> errors_generator = new ArrayList<>();
+
 //    for epoch in range(epochs):
+    for (int i = 0; i < epochs; i++) {
+
 //  face: ndarray
 //    for face in faces:  # faces = list of ndarrays
+      for (double[] face : faces) {
 //        # Update the discriminator weights from the real face
 //        D.update_from_image(face)
-//    
+        d.update_from_image(face);
+
 //    # Pick a random number to generate a fake face
 //  z: float = random.rand()
-//    
+        double z = rand.nextGaussian();
+
 //    # Calculate the discriminator error
 //        errors_discriminator.append(sum(D.error_from_image(face) + D.error_from_noise(z)))
-//    
+        double a = d.error_from_image(face);//float64
+        double[] b = d.error_from_noise(z);//ndarray
+        double[] c = new double[b.length];
+        for (int j = 0; j < b.length; j++) {
+          c[j] = a + b[j];
+        }
+        errors_discriminator.add(sum(c));
+
 //    # Calculate the generator error
 //        errors_generator.append(G.error(z, D))
-//    
+        errors_generator.add(g.error(z, d));
+
 //    # Build a fake face
 //  noise: ndarray = G.forward(z)
-//    
+        double[] noise = g.forward(z);
+
 //    # Update the discriminator weights from the fake face
 //        D.update_from_noise(noise)
-//    
+        d.update_from_noise(noise);
+
 //    # Update the generator weights from the fake face
 //        G.update(z, D)
+        g.update(z, d);
+      }
+    }
+    
+    new Window(g, errors_discriminator, errors_generator);
+  }
+  
+  static class Window extends JPanel {
+    private final static Font FONT = new Font("Arial", Font.BOLD, 20);
+    private final static int SAMPLES_PER_ROW = 3;
+    private final static int SAMPLE_PADDING = 10;
+    Generator generatorInstance;
+    List<Double> errors_discriminator;
+    List<Double> errors_generator;
+  
+    public Window(Generator generatorInstance, List<Double> errors_discriminator, List<Double> errors_generator) {
+      this.generatorInstance = generatorInstance;
+      this.errors_discriminator = errors_discriminator;
+      this.errors_generator = errors_generator;
+      
+      // Creating a JFrame to make the JPanel double buffered
+      JFrame frame = new JFrame("Slanted Land Java");
+      frame.setLayout(null);
+      frame.setSize(1200, 600);
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setVisible(true);
+      frame.setLocationRelativeTo(null);
+      frame.setBackground(new Color(117, 117, 117));
+      frame.add(this);
+      
+      setSize(frame.getWidth(), frame.getHeight());
+      
+      setDoubleBuffered(true);
+      
+      JButton buttonSamples = new JButton("Press for samples");
+      buttonSamples.addActionListener(e -> drawSamples());
+      buttonSamples.setLocation(20, getHeight() - 80);
+      buttonSamples.setSize(150, 30);
+      frame.add(buttonSamples);
+    }
+    
+    void drawSamples() {
+      JFrame frame = new JFrame("samples");
+      JPanel panel = new JPanel() {
+        public void paint(Graphics graphics) {
+          setDoubleBuffered(true);
+          graphics.setColor(new Color(155, 155, 117));
+          graphics.fillRect(0, 0, getWidth(), getHeight());
+          int padding = 10;
+          int size = getWidth() / SAMPLES_PER_ROW;
+          for (int x = 0; x < SAMPLES_PER_ROW; x++) {
+            for (int y = 0; y < SAMPLES_PER_ROW; y++) {
+              double z = rand.nextGaussian();
+              double[] generated_image = generatorInstance.forward(z);
+              int[] pixels = doubleValuesToPixels(generated_image);
+              
+              BufferedImage image = new BufferedImage(2, 2, BufferedImage.TYPE_INT_RGB);
+              for (int i = 0; i < pixels.length; i++) {
+                image.setRGB(i % 2, i / 2, new Color(pixels[i], pixels[i], pixels[i]).getRGB());
+              }
+              graphics.drawImage(image,
+                x * size + x * padding + padding,
+                y * size + y * padding + padding,
+                size - padding * 4,
+                size - padding * 4,
+                null
+              );
+            }
+          }
+        }
+      };
+      frame.add(panel);
+      frame.setSize(600, 600);
+      frame.setVisible(true);
+      frame.setLocationRelativeTo(null);
+    }
+    
+    int[] doubleValuesToPixels(double[] input) {
+      int[] ints = new int[input.length];
+      for (int i = 0; i < input.length; i++) {
+        ints[i] = (int) (input[i] * 255d);
+      }
+      return ints;
+    }
+    
+    @Override
+    public void paint(Graphics graphics) {
+      super.paint(graphics);
+      
+      double halfWidth = getWidth() / 2d - SAMPLE_PADDING * 2;
+      double heightScale = getHeight() / 4;
+      double size = halfWidth / errors_discriminator.size();
+      graphics.setColor(Color.blue);
+      
+      int[] x = new int[errors_generator.size()];
+      int[] y = new int[errors_generator.size()];
+      for (int i = 0; i < errors_generator.size(); i++) {
+        x[i] = (int) (i * size) + SAMPLE_PADDING;
+        y[i] = invertY(errors_generator.get(i) * heightScale);
+      }
+      graphics.drawPolyline(x, y, x.length);
+      
+      x = new int[errors_discriminator.size()];
+      y = new int[errors_discriminator.size()];
+      for (int i = 0; i < errors_discriminator.size(); i++) {
+        x[i] = (int) (i * size + halfWidth) + SAMPLE_PADDING * 3;
+        y[i] = invertY(errors_discriminator.get(i) * heightScale);
+      }
+      graphics.drawPolyline(x, y, x.length);
+      
+      graphics.setColor(Color.black);
+      graphics.setFont(FONT);
+      graphics.drawString("Generator", SAMPLE_PADDING, 50);
+      graphics.drawString("Discriminator", getWidth() / 2 + SAMPLE_PADDING * 2, 50);
+    }
+    
+    int invertY(double y) {
+      return (int) (getHeight() - y);
+    }
+  }
 }
